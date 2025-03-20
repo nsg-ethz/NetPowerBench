83f0ca41249d2741f90efc27cc661c8d730ec577
Priska Steinebrunner <priska@soi.ch>
2025-03-20 11:19:06 +0100

    Started new helper function file

--- starting_point/automation/helper_functions.py
+++ starting_point/automation/helper_functions.py
@@ -2,3 +2,20 @@ def print_experiment_duration(time_per_run_s, number_of_runs):
     """
     Prints the expected duration of the experiment
     """
+
+def get_log_path(metadata):
+    """
+    Returns the path including file name for logging the experiment
+    """
+
+    # Code currently in measure_and_store
+
+def start_traffic(metadata):
+    """
+    Starts traffic generation
+    """
+
+def stop_traffic(metadata, traffic_gen_process):
+    """
+    Stops traffic generation
+    """
