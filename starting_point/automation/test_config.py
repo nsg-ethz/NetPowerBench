ebe42a4cf0865da838e9e8d1534dd9cd843d94f1
Priska Steinebrunner <psteinebrunn@student.ethz.ch>
2025-04-09 16:51:16 +0200

    Created new file and mapped out a script for testing port configurations

---
+++ starting_point/automation/test_config.py
@@ -0,0 +1,10 @@
+from test_main import *
+
+# Load port list
+# Put everything needed into metadata
+# Reset all ports 
+# Get User confirmation that all ports are off
+# Chose a random set of port with possibility of controlled randomization
+# Get user confirmation for config 
+# Configure ports for snake test
+# Get user confirmation
