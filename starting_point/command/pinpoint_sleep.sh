a3244037b1aac9a1cfe075b710971d1b52fd384a
Priska Steinebrunner <psteinebrunn@student.ethz.ch>
2025-05-15 16:06:24 +0200

    Changed command back to use counter 0

--- starting_point/command/pinpoint_sleep.sh
+++ starting_point/command/pinpoint_sleep.sh
@@ -14,7 +14,7 @@ logname="../data/log/pinpoint.log"
 /bin/rm $logname
 
 # Run pinpoint
-# $PATH -c -e MCP1,MCP2 -i $SAMPLING_INTERVAL -b -$START_UP_DELAY -o  ../data/log/pinpoint.log --header -- /bin/sleep  $DURATION
+$PATH -c -e MCP1,MCP2 -i $SAMPLING_INTERVAL -b -$START_UP_DELAY -o  ../data/log/pinpoint.log --header -- /bin/sleep  $DURATION
 # -> currently there is a bug where the header does not get written to the output, so just leave it out.
 # $PATH -c -e MCP1,MCP2 -i $SAMPLING_INTERVAL -b -$START_UP_DELAY -o  ../data/log/pinpoint.log -- /bin/sleep  $DURATION
-$PATH -c -e mcp:dev1ch1,mcp:dev1ch2 -i $SAMPLING_INTERVAL -b -$START_UP_DELAY -o  ../data/log/pinpoint.log -- /bin/sleep  $DURATION
+# $PATH -c -e mcp:dev1ch1,mcp:dev1ch2 -i $SAMPLING_INTERVAL -b -$START_UP_DELAY -o  ../data/log/pinpoint.log -- /bin/sleep  $DURATION
