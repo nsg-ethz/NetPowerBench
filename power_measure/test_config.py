abaadfaed55c6d21e33ca95689b3f0df23ec641d
Priska Steinebrunner <psteinebrunn@student.ethz.ch>
2025-06-11 18:20:42 +0200

    Fixed bug in test_config.py

--- power_measure/test_config.py
+++ power_measure/test_config.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
         if usr1.lower() == 'n':
             sys.exit(0)
 
-    if 'full_reset':
+    if 'full_reset' in tests:
         # Disabling all ports
         cmd = get_port_config(metadata, 'disable_all')
         configure_ports(metadata, cmd)
